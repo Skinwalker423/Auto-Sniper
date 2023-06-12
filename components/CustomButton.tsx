1f896103150335db31519193b56c9a98f6a2ca50
Luis Gonzalez <lgonzalez23@verizon.net>
2023-06-12 12:45:46 -0700

    handleScroll funciton

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,21 +1,26 @@
 "use client";
-import React from "react";
+import React, {
+  EventHandler,
+  MouseEventHandler,
+} from "react";
 
 type CustomButtonProps = {
   title: string;
   containerStyles: string;
+  handleClick: MouseEventHandler;
 };
 
 const CustomButton = ({
   title,
   containerStyles,
+  handleClick,
 }: CustomButtonProps) => {
   return (
     <button
       className={`custom-btn ${containerStyles}`}
       disabled={false}
       type={"button"}
-      onClick={() => {}}
+      onClick={handleClick}
     >
       <span className={`flex-1`}>{title}</span>
     </button>
