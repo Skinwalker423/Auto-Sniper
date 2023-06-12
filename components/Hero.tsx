57082c1dc64ad5c0277ec94e71fb2906aee9e2b5
Luis Gonzalez <lgonzalez23@verizon.net>
2023-06-12 13:46:45 -0700

    metadata in layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,10 +27,11 @@ const Hero = () => {
         <div className='hero__image'>
           <Image
             src={"/hero.png"}
-            width={500}
-            height={500}
+            fill
             alt='hero image'
+            className='object-contain'
           />
+          <div className='hero__image-overlay' />
         </div>
       </div>
     </div>
