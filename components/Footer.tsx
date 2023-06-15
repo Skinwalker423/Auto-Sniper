fe015b61d71259008fe03be620a163261e000264
Luis Gonzalez <lgonzalez23@verizon.net>
2023-06-15 12:31:55 -0700

    footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { footerLinks } from "@/constants";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='flex flex-col text-black-100 mt-5 border-t border-gray-100'>
+      <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+        <div className='flex flex-col justify-start items-center gap-6'>
+          <Image
+            src={"/logo.svg"}
+            width={118}
+            height={18}
+            alt='logo'
+            className='object-contain'
+          />
+          <p className='text-base text-gray-700'>
+            Auto Sniper 2023 <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+        <div className='footer__links'>
+          {footerLinks.map((link) => {
+            return (
+              <div
+                key={link.title}
+                className='footer__link'
+              >
+                <h3 className='font-bold text-xl'>
+                  {link.title}
+                </h3>
+                {link.links.map(({ title, url }) => {
+                  return (
+                    <Link
+                      key={title}
+                      className='text-base text-gray-500 my-1 hover:text-gray-900'
+                      href={url}
+                    >
+                      {title}
+                    </Link>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
