2f44552c69fdd295343f2ff20c2c6ce783732232
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-17 16:29:25 -0700

    SearchManufacturerProps

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import React from "react";
-import SearchManufacturer from "./SearchManufacturer";
+import React, { useState } from "react";
+import { SearchManufacturer } from "./";
 
 const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("");
   const handleSearch = () => {};
   return (
     <form onSubmit={handleSearch} className='searchbar'>
       <div className='search__item'>
-        <SearchManufacturer />
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
       </div>
     </form>
   );
--- types/index.ts
+++ types/index.ts
@@ -2,7 +2,14 @@ import {
   MouseEventHandler,
   ButtonHTMLAttributes,
   DetailedHTMLProps,
+  Dispatch,
+  SetStateAction,
 } from "react";
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
 
 export interface CustomButtonProps {
   title: string;
