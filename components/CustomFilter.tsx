1513d7713e91627fba2515de221a43131c36f26d
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-17 16:04:00 -0700

    CustomFilter and SearchBar base component

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface CustomFilterProps {
+  title: string;
+}
+
+const CustomFilter = ({ title }: CustomFilterProps) => {
+  return <div>CustomFilter</div>;
+};
+
+export default CustomFilter;
