b33fa3b38bdfc17808c240f316a90a1810c75cb8
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-19 14:29:07 -0700

    calculate car rent

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,9 +1,27 @@
 "use client";
 import { CarCardProps } from "@/types";
 import React from "react";
+import CustomButton from "./CustomButton";
 
 const CarCard = ({ car }: CarCardProps) => {
-  return <div>CarCard</div>;
+  const {
+    city_mpg,
+    year,
+    make,
+    model,
+    transmission,
+    drive,
+  } = car;
+
+  return (
+    <div className='car-card group'>
+      <div className='car-card__content'>
+        <h2 className='car-card__content-title'>
+          {make} {model}
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default CarCard;
