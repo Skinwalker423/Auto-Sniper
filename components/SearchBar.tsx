84d7078311c8d63f4409b215d839ff9922c16d59
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-21 20:29:15 -0700

    searchButton

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,23 +4,47 @@ import { FormEvent, FormEventHandler } from "react";
 
 import React, { useState } from "react";
 import { SearchManufacturer } from "./";
+import Image from "next/image";
 
-const SearchBar = () => {
+export const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
   const handleSearch = (e: FormEvent<HTMLInputElement>) => {
     e.preventDefault();
     console.log("form submitted");
   };
+
+  const SearchButton = ({
+    otherClasses,
+  }: {
+    otherClasses?: string;
+  }) => {
+    return (
+      <button
+        type='button'
+        className={`-ml-3 z-10 ${otherClasses}`}
+      >
+        <Image
+          src='/magnifying-glass.svg'
+          alt='magnifying glass'
+          width={40}
+          height={40}
+          className='object-contain'
+        />
+      </button>
+    );
+  };
+
   return (
     <form
       onSubmit={handleSearch as FormEventHandler}
       className='searchbar'
     >
-      <div className='search__item'>
+      <div className='searchbar__item'>
         <SearchManufacturer
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses='sm:hidden' />
       </div>
     </form>
   );
