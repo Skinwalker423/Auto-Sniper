123847eefc123c64bd2cf306466994fd23019d82
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-23 15:13:00 -0700

    ListBox Options

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,20 +1,63 @@
 "use client";
 
 import { CustomFilterProps } from "@/types";
-import { Listbox } from "@headlessui/react";
-import React from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import Image from "next/image";
+import React, { Fragment, useState } from "react";
 
 const CustomFilter = ({
   title,
   options,
 }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div className='w-fit'>
-      <Listbox>
+      <Listbox
+        value={selected}
+        onChange={(e) => setSelected(e)}
+      >
         <div className='relative w-fit z-10'>
           <Listbox.Button className={"custom-filter__btn"}>
-            {title}
+            <span className='block truncate'>
+              {selected.title}
+            </span>
+            <Image
+              src={"/chevron-up-down.svg"}
+              width={20}
+              height={20}
+              alt='chevron up/down'
+            />
           </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opactity-100'
+            leaveTo='opacity-0'
+          >
+            <Listbox.Options
+              className={"custom-filter__options"}
+            >
+              {options.map((option) => {
+                return (
+                  <Listbox.Option
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 px-4 ${
+                        active
+                          ? "bg-primary-blue text-white"
+                          : "text-gray-900"
+                      }`
+                    }
+                    key={option.title}
+                    value={option}
+                  >
+                    {({ selected }) => {
+                      return <span>{option.title}</span>;
+                    }}
+                  </Listbox.Option>
+                );
+              })}
+            </Listbox.Options>
+          </Transition>
         </div>
       </Listbox>
     </div>
